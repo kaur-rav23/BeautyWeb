01a74aa6dd044c9e1ff51413cd5fbe2e8b4d6953
Ravleen Kaur <102215556+kaur-rav23@users.noreply.github.com>
2025-04-09 16:38:59 +0530

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="dark-footer">
+      <h2>Go&Glow</h2>
+      <p>
+      Go and Glow – Your one-stop destination for radiant beauty and premium cosmetic essentials.
+      </p>
+      <div className="social-icons">
+        <a href="#"><i className="fab fa-facebook-f"></i></a>
+        <a href="#"><i className="fab fa-twitter"></i></a>
+        <a href="#"><i className="fab fa-google-plus-g"></i></a>
+        <a href="#"><i className="fab fa-youtube"></i></a>
+        <a href="#"><i className="fab fa-linkedin-in"></i></a>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
