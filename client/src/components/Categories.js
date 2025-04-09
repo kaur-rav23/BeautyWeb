0450ccaff4bf0b4b81f8318cffc7dd85bf77017c
Ravleen Kaur <102215556+kaur-rav23@users.noreply.github.com>
2025-04-09 16:38:09 +0530

    Update Categories.js

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Categories.css";
 import data from '../utils/mockData';
+import { useNavigate } from "react-router-dom";
 const Categories = () => {
     const uniqueProductTypes = [...new Set(data.map((item) => item.product_type))];
+    const navigate=useNavigate('');
+    const handleShopNow=()=>{
+        navigate('./products');
+    }
     return (
         <div>
             <div className="heading-div"> <h2 className="categories-heading" style={{"textAlign":"center"}}>Discover Our Categories</h2></div>
@@ -13,7 +18,7 @@ const Categories = () => {
                         <div className="category-content">
                             <p className="category-offer">{category.offer}</p>
                             <h3 className="category-name">{category}</h3>
-                            <button className="shop-now">Shop Now</button>
+                            <button className="shop-now" onClick={handleShopNow}>Shop Now</button>
                         </div>
                         <img src="https://images.pexels.com/photos/234220/pexels-photo-234220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="bg-image" className="category-image" />
                     </div>
